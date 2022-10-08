b8798d62479f76b74ed262e25081e363c20ab4ad
root <mweuc@localhost.localdomain>
2022-10-08 06:37:06 -0400

    5th commit

--- MiwaniPambeTz/settings.py
+++ MiwaniPambeTz/settings.py
@@ -196,9 +196,10 @@ EMAIL_HOST_PASSWORD = 'opkstfrgmmlsgrhk'  # this is app password for my gmail ht
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 AUTH_USER_MODEL = 'Register.CustomUser'
